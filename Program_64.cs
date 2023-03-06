3bff6f45cf96f809472d6c63f9fe33800601ca52
dyanov29 <123237209+dyanov29@users.noreply.github.com>
2023-03-06 09:51:43 +0300

    Add files via upload

---
+++ Program_64.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+            int n= int.Parse(Console.ReadLine());
+            f(n);
+            Console.Read();
+static void f(int n)
+        {
+            Console.WriteLine(n);
+            n-=1;
+            if(n>0)
+                f(n);
+        }
---
+++ Program_66.cs
@@ -0,0 +1,14 @@
+﻿int summ(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			return (end + start)*(end - start + 1)/2;
+		}
+		
+		Console.WriteLine(summ(1, 15));
+	
